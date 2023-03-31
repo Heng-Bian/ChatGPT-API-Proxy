27e15468c290c7d861b0a3a78d88d715933869e0
Heng-Bian <bianheng@aliyun.com>
2023-03-31 13:32:06 +0800

    optimizing

--- main.go
+++ main.go
@@ -9,13 +9,15 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 	"sync"
 	"sync/atomic"
 )
 
 var (
-	port       = flag.String("port", "8080", "Port to listen on")
+	port       = flag.Int("port", 8080, "Port to listen on")
 	auth       = flag.String("auth", "", "Authorization Header of reverse proxy")
 	target     = flag.String("target", "https://api.openai.com", "ChatGPT API address")
 	tokenstr   = flag.String("tokens", "", "Comma separated ChatGPT API tokens")
@@ -23,54 +25,26 @@ var (
 )
 
 var count int64
-var lock sync.RWMutex
 var tokens []string
+
+var scheme string
+var host string
+
+var lock sync.RWMutex
 
 func main() {
 	loadConfig()
+	sort.Strings(tokens)
 	url, err := url.Parse(*target)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
+	scheme = url.Scheme
+	host = url.Host
+
 	proxy := &httputil.ReverseProxy{
-		Director: func(req *http.Request) {
-			lock.RLock()
-			var token string
-			if len(tokens) > 0 {
-				token = tokens[count%int64(len(tokens))]
-			}
-			lock.RUnlock()
-			atomic.AddInt64(&count, 1)
-			req.URL.Scheme = url.Scheme
-			req.URL.Host = url.Host
-			req.Host = url.Host
-			req.Header.Del("Authorization")
-			req.Header.Add("Authorization", "Bearer "+token)
-		},
-		ModifyResponse: func(r *http.Response) error {
-			if r.StatusCode != 401 {
-				return nil
-			}
-			au := r.Request.Header.Get("Authorization")
-			if strings.HasPrefix(au, "Bearer ") {
-				token := strings.Split(au, " ")[1]
-				lock.Lock()
-				defer lock.Unlock()
-				for i, value := range tokens {
-					if token == value {
-						//end of the slice
-						if i == len(tokens)-1 {
-							tokens = tokens[:i]
-						} else {
-							tokens = append(tokens[:i], tokens[i+1:]...)
-						}
-						log.Println("ChatGPT API token " + token + " invalid and has been evicted")
-						break
-					}
-				}
-			}
-			return nil
-		},
+		Director:       director,
+		ModifyResponse: modifyResponse,
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -82,15 +56,67 @@ func main() {
 		proxy.ServeHTTP(w, r)
 	})
 
-	log.Println("Listen on port:" + *port)
+	log.Println("Listen on port:" + strconv.Itoa(*port))
 	log.Println("Running...")
-	err = http.ListenAndServe(":"+*port, nil)
+	err = http.ListenAndServe(":"+strconv.Itoa(*port), nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
+func director(req *http.Request) {
+	lock.RLock()
+	var token string
+	if len(tokens) > 0 {
+		token = tokens[count%int64(len(tokens))]
+	}
+	lock.RUnlock()
+	atomic.AddInt64(&count, 1)
+	req.URL.Scheme = scheme
+	req.URL.Host = host
+	req.Host = host
+	req.Header.Del("Authorization")
+	if token != "" {
+		req.Header.Add("Authorization", "Bearer "+token)
+	}
+}
+
+func modifyResponse(r *http.Response) error {
+	if r.StatusCode != 401 {
+		return nil
+	}
+	//evict the invalid token
+	au := r.Request.Header.Get("Authorization")
+	if !strings.HasPrefix(au, "Bearer ") {
+		return nil
+	}
+	token := strings.Split(au, " ")[1]
+	lock.Lock()
+	defer lock.Unlock()
+	tokens = findAndRemove(tokens, token)
+	log.Println("ChatGPT API token " + token + " invalid and has been evicted")
+	return nil
+}
+
+func findAndRemove(sorted []string, str string) []string {
+	i := sort.SearchStrings(sorted, str)
+	if i != len(sorted) && str == sorted[i] {
+		if i == len(sorted)-1 {
+			sorted = sorted[:i]
+		} else {
+			sorted = append(sorted[:i], sorted[i+1:]...)
+		}
+	}
+	return sorted
+}
+
 func loadConfig() {
+	type config struct {
+		Port   int      `json:"port"`
+		Auth   string   `json:"auth"`
+		Target string   `json:"target"`
+		Tokens []string `json:"tokens"`
+	}
 	flag.Parse()
 	splits := strings.Split(*tokenstr, ",")
 	for _, value := range splits {
@@ -104,41 +130,23 @@ func loadConfig() {
 		if err != nil {
 			log.Fatalln(err)
 		}
-		configMap := map[string]interface{}{}
-		err = json.NewDecoder(file).Decode(&configMap)
+		var config config
+		err = json.NewDecoder(file).Decode(&config)
 		if err != nil {
 			log.Fatalln(err)
 		}
-		if po := configMap["port"]; po != nil {
-			poValue, ok := po.(string)
-			if ok {
-				port = &poValue
-			} else {
-				log.Fatalln("port in config.json must be string")
-			}
+		//overwrite command line
+		if config.Port != 0 {
+			port = &config.Port
 		}
-		if au := configMap["auth"]; au != nil {
-			auValue, ok := au.(string)
-			if ok {
-				auth = &auValue
-			}
+		if config.Auth != "" {
+			auth = &config.Auth
 		}
-		if ta := configMap["target"]; ta != nil {
-			taValue, ok := ta.(string)
-			if ok {
-				target = &taValue
-			}
+		if config.Target != "" {
+			target = &config.Target
 		}
-		if to := configMap["tokens"]; to != nil {
-			toValue, ok := to.([]interface{})
-			if ok {
-				for _, k := range toValue {
-					t, ok := k.(string)
-					if ok {
-						tokens = append(tokens, t)
-					}
-				}
-			}
+		if len(config.Tokens) > 0 {
+			tokens = config.Tokens
 		}
 	}
 }
